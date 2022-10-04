3548a2a9b7caa875ab99ed551a56397a94bb51ec
smol-cat <ernestasunt@gmail.com>
2022-10-04 19:32:29 +0300

    Database fetch tools, account controller

--- server/Program.cs
+++ server/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddCors(c => )
 
 var app = builder.Build();
 
@@ -15,8 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
